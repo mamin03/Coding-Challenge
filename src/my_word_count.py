87d1f95a3f2f3ad5daaf9117a9d70c5d729fea2d
Muhamed A Amin <mamin03@psexport01.slac.stanford.edu>
2015-03-04 13:09:58 -0800

    word count

--- src/my_word_count.py
+++ src/my_word_count.py
@@ -1,12 +1,17 @@
 import sys
-file=open(sys.argv[0],"r+")
-wordcount={}
-for word in file.read().split():
-    if word not in wordcount:
-        wordcount[word] = 1
-    else:
-        wordcount[word] += 1
-for key in wordcount.keys():
-  print ("%s %s " %(key , wordcount[key]))
-file.close();
 
+arg=sys.argv
+if len(arg)>0:
+
+   file=open(sys.argv[0],"r+")
+   wordcount={}
+   for word in file.read().split():
+       if word not in wordcount:
+           wordcount[word] = 1
+       else:
+           wordcount[word] += 1
+   for key in wordcount.keys():
+       print ("%s %s " %(key , wordcount[key]))
+   file.close();
+else:
+   print "Please provide the input file"
