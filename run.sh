47a029d923ede749ac150c61baa27908e259cc64
mamin03 <siasas@yahoo.com>
2015-03-05 18:28:31 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # example of the run script for running the word count
 # first I'll load all my dependencies
-#apt-get install Python2.7
+apt-get install Python2.7
 # next I'll make sure that all my programs (written in Python in this example) have the proper permissions
 chmod a+x src/my_word_count.py
 chmod a+x src/my_running_median.py
