fee3c600a326dd36118456b62d393186dc002ee2
Muhamed A Amin <mamin03@psexport01.slac.stanford.edu>
2015-03-05 14:46:00 -0800

    code challenge

--- src/my_running_median.py
+++ src/my_running_median.py
@@ -2,14 +2,14 @@ from numpy import median
 import sys
 
 arg=sys.argv
-if len(arg)>0:
-  try:
-     read=open(arg[1])
-     write_output=open(arg[2], "w+")
-     a=[]
-     for i in read:
-       a.append(len(i.split()))
-       write_output.write(str(median(a))+'\n')
+if len(arg)>0: # check if the input and out files provided
+  try:  
+     read=open(arg[1]) #  open the input file for reading
+     write_output=open(arg[2], "w+") # open the output file for writing
+     a=[]              # Initialize an array to carry the number of words in each line 
+     for i in read:    # loop over all lines
+       a.append(len(i.split())) # add the number of words
+       write_output.write(str(median(a))+'\n') # write the median of updated array
      print "output saved in", arg[2]
   except: # catch *all* exceptions
     e = sys.exc_info()[1]
--- src/my_word_count.py
+++ src/my_word_count.py
@@ -2,20 +2,20 @@ import sys
 import re
 
 arg=sys.argv
-if len(arg)>0:
+if len(arg)>2:   # check if the input and out files provided
   try:
-    file=open(arg[1],"r+")
-    wordcount={}
-    all_words=file.read()
-    non_punctuation = re.sub('[^a-zA-Z0-9]', ' ', all_words)
-    for word in non_punctuation.split():
-       if word not in wordcount:
-           wordcount[word] = 1
-       else:
-           wordcount[word] += 1
-    file.close();    
-    write_output=open(arg[2], "w+")
-    for key in wordcount.keys():
+    file=open(arg[1]) # open the input file for reading
+    wordcount={}      # dictionary that carries the words count
+    all_words=file.read() # read the file and load it in a string
+    non_punctuation = re.sub('[^a-zA-Z0-9]', ' ', all_words) # remove punctuations
+    for word in non_punctuation.split(): # loop over all words
+       if word not in wordcount: # if the word does not exist
+           wordcount[word] = 1   # add a new key and initialize the value to 1
+       else:                     # if the word exists
+           wordcount[word] += 1  # update the value
+    file.close();                # close the file
+    write_output=open(arg[2], "w+") # open the output file for writing
+    for key in wordcount.keys(): # write the output 
        write_output.write("%s %s \n" %(key , wordcount[key]))
     print "output saved in", arg[2]
   except: # catch *all* exceptions
